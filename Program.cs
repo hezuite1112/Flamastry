692764aeae0471c509c63282c0f5e3c55d76e1a4
hezuite1112 <30926270+hezuite1112@users.noreply.github.com>
2020-02-05 19:51:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flamastry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string C;
+            C = Console.ReadLine();
+
+            for(int i=0; i<int.Parse(C); i++)
+            { 
+                string tekst = Console.ReadLine();
+                string wynik = "";
+                int ile = 1, j = 0;
+                while(j<tekst.Length)
+                {
+                    ile = 1;
+                    char aktualnyZnak = tekst[j];
+                    j++;
+                    for (; j < tekst.Length && tekst[j] == aktualnyZnak; j++)
+                    {
+                        ile++;
+                    }
+                    if (ile ==1)
+                    {
+                        wynik += aktualnyZnak.ToString();
+                    }
+                    else if (ile ==2)
+                    {
+                        wynik += aktualnyZnak.ToString();
+                        wynik += aktualnyZnak.ToString();
+                    }
+                    else
+                    {
+                        wynik += aktualnyZnak.ToString() + ile.ToString();
+                    }
+                }
+                Console.WriteLine(wynik);
+            
+            }
+        }
+    }
+}
